7ca2b8c3d0aadce907d217a65e1ab985d2376e7e
kxbk <572472782@qq.com>
2019-10-22 20:04:52 +0800

    Initial commit

---
+++ service-ribbon/src/test/java/com/kxb/serviceribbon/ServiceRibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kxb.serviceribbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRibbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
